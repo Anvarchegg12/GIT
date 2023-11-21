01c30a03f3f7db865f91b5d7c92abfabd68fe55e
Milordius <151358199+Milordius@users.noreply.github.com>
2023-11-21 23:34:10 +0500

    Update main.c

--- main.c
+++ main.c
@@ -5,13 +5,13 @@ int main(int argc, char **argv)
 {
     char first[255], last[255];
 
-    printf("Enter your first name: ");
+    printf("Введи имя: ");
     fgets(first, 255, stdin);
     first[strlen(first)-1] = '\0'; /* remove the newline at the end */
     
-    printf("Now enter your last name: "); 
+    printf("А теперь фамилию: "); 
     gets(last);  /* buffer overflow? what's that? */
     
-    printf("Hello &s &s!\n", first, last);
+    printf("Привет &s &s!\n", first, last);
     return 0;
 }
